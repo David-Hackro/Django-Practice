1224716dca09db013addc9f781dc94a098cceb7d
David hackro <davidhackro@MacBook-Pro-de-David.local>
2024-02-16 06:25:21 -0600

    add confitionales with jinja

--- myapp/models.py
+++ myapp/models.py
@@ -10,6 +10,7 @@ class Project(models.Model):
 class Task(models.Model):
     title = models.CharField(max_length = 200, default="")
     description = models.TextField(default="")
+    done = models.BooleanField(default = False)
     project = models.ForeignKey(Project, on_delete = models.CASCADE)
     def __str__(self):
         return self.title + ' - ' + self.project.name
